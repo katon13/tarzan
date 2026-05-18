d0c377de3197d73178321b68c4799e46460dca57
Paweł Zastrzeżyński <zastrzezynski.pawel@gmail.com>
2026-05-18 09:07:57 +0200

    Stabilna wersja czysta po usunieciu zbednych plikow

--- font/editor/EHR/tarzanEhrApp.py
+++
@@ -1,7 +0,0 @@
-from __future__ import annotations
-
-# Shim zgodności importów. UI Tkinter EHR jest teraz w editor/tarzanEHR.py.
-
-from editor.EHR.tarzanEhrUi import AxisSettingsDialog, TarzanEhrMultiAxisWindow, main
-
-__all__ = ["AxisSettingsDialog", "TarzanEhrMultiAxisWindow", "main"]
